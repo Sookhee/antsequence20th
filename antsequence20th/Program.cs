40e71493a14a166d72f66780cf08283f4ca8cadd
Sookhee <s2018s38@e-mirim.hs.kr>
2020-05-12 12:34:07 +0900

    개미 수열 반복문 이용하여 구현

--- antsequence20th/Program.cs
+++ antsequence20th/Program.cs
@@ -10,6 +10,39 @@ namespace antsequence20th
     {
         static void Main(string[] args)
         {
+            int[] antsequence = new int[1000];
+            int[] tempArray = new int[1000];
+            antsequence[0] = 1;
+            int count = 1, j = 0, tempIndex = 0;
+
+            int loop = 20;
+            for (int i = 1; i < loop; i++)
+            {
+                while (antsequence[j] != 0)
+                {
+                    if (antsequence[j] == antsequence[j + 1])
+                    {
+                        count++;
+                    }
+                    else
+                    {
+                        tempArray[tempIndex++] = antsequence[j];
+                        tempArray[tempIndex++] = count;
+                        count = 1;
+                    }
+                    j++;
+                }
+                //antsequence = (int[])tempArray.Clone();
+                Array.Copy(tempArray, antsequence, tempArray.Length);
+                j = 0; tempIndex = 0;
+            }
+
+            //출력
+            foreach (var item in antsequence)
+            {
+                if (item == 0) break;
+                Console.Write(item);
+            }
         }
     }
 }
